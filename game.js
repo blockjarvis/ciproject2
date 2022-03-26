56f958a2a60fb8aae3330e018094e4dab89c2555
Rafael Correa <tiobenedito@gmail.com>
2022-03-26 21:57:03 +0000

    pairsleft and win alert

--- game.js
+++ game.js
@@ -2,11 +2,12 @@ const cards = document.querySelectorAll('.card');
 let lock = false;
 let hasTurnedCard = false;
 let firstCard, secondCard;
+let pairsLeft = 10;
 
 function onClick() {
     if (lock) return;
     if (this === firstCard) return;
-console.log("click")
+    console.log("click")
     this.classList.toggle('turn');
     if (!hasTurnedCard) {
         hasTurnedCard = true;
@@ -18,17 +19,19 @@ console.log("click")
     secondCard = this;
     checkForPair();
 }
-    cards.forEach(card => card.addEventListener('click', onClick));
+cards.forEach(card => card.addEventListener('click', onClick));
 
 /*-- ===== check pair found ==== -- */
 function checkForPair() {
     let isMatch = firstCard.dataset.type === secondCard.dataset.type;
     isMatch ? disableCards() : turnBackCards();
+    checkEndGame();
 }
 
 function disableCards() {
     firstCard.removeEventListener('click', onClick);
     secondCard.removeEventListener('click', onClick);
+    pairsLeft--;
 }
 
 /*-- ===== Unturn card ==== -- */
@@ -49,3 +52,10 @@ function shuffle() {
     });
 }
 shuffle();
+
+/*-- ===== Check End Game ==== -- */
+function checkEndGame() {
+    if (pairsLeft == 0) {
+        alert("Nice One!");
+    }
+}
